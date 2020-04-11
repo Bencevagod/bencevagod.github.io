a67288bda93b8e7c37a6eadbeda80ba12dc71f32
Bence Bajkai <bajkaibence@gmail.com>
2020-04-11 19:26:59 +0200

    without jQuery

--- counter.js
+++ counter.js
@@ -1,4 +1,24 @@
+//Ez egy számláló webalkalmazás
+
+//új kód jQuery nélkül
 let num = 0;
+let incr = document.getElementById('increase');
+let decr = document.getElementById('decrease');
+let printed = document.getElementById('number');
+
+incr.onclick=function() {
+    num++;
+    printed.textContent=num;
+}
+
+decr.onclick=function() {
+    num--;
+    printed.textContent=num;
+}
+
+
+/*
+Régi kód jQueryvel
 $('#increase').click(() => {
     num++;
     $('#number').text(num);
@@ -8,3 +28,4 @@ $('#decrease').click(() => {
     num--;
     $('#number').text(num);
 })
+*/
